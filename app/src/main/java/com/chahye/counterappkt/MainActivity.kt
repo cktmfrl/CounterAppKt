cc7c4f87e46e74def13224470026d9deab96bbaa
origin <sskkccha@gmail.com>
2022-09-26 15:15:19 +0900

    시스템에 의한 종료 시 UI 상태 유지 처리

--- app/src/main/java/com/chahye/counterappkt/MainActivity.kt
+++ app/src/main/java/com/chahye/counterappkt/MainActivity.kt
@@ -63,7 +63,17 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
+    }
 
+    // 시스템에 의한 종료 시 UI 상태 유지
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("count", viewModel.count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.count = savedInstanceState.getInt("count")
     }
 
     companion object {
