908919341e2b1efc2e24ba938eb789d2eab2f1d8
origin <sskkccha@gmail.com>
2022-09-26 15:24:24 +0900

    LiveData 적용

--- app/src/main/java/com/chahye/counterappkt/MainActivity.kt
+++ app/src/main/java/com/chahye/counterappkt/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,16 +18,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        count_text_view.text = "${viewModel.count}"
+        // UI 갱신
+        viewModel.countLiveData.observe(this, Observer { count ->
+            count_text_view.text = "$count"
+        })
 
         add_button.setOnClickListener {
-            viewModel.count++
-            count_text_view.text = "${viewModel.count}"
+            viewModel.increaseCount()
         }
 
         sub_button.setOnClickListener {
-            viewModel.count--
-            count_text_view.text = "${viewModel.count}"
+            viewModel.decreaseCount()
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -66,15 +68,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     // 시스템에 의한 종료 시 UI 상태 유지
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", viewModel.count)
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        viewModel.count = savedInstanceState.getInt("count")
-    }
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putInt("count", viewModel.count)
+//    }
+//
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//        viewModel.count = savedInstanceState.getInt("count")
+//    }
 
     companion object {
         val TAG = MainActivity::class.java.simpleName
--- app/src/main/java/com/chahye/counterappkt/MainViewModel.kt
+++ app/src/main/java/com/chahye/counterappkt/MainViewModel.kt
@@ -1,7 +1,20 @@
 package com.chahye.counterappkt
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
-    var count = 0
+    private var count = 0
+
+    val countLiveData = MutableLiveData<Int>()
+
+    fun increaseCount() {
+        count++
+        countLiveData.value = count
+    }
+
+    fun decreaseCount() {
+        count++
+        countLiveData.value = count
+    }
 }
